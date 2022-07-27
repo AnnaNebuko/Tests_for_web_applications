c9287f6b149ab283b8f2d1c0cec66b1ec5438ac9
Anna Nebuko <Anna_Nebuko@epam.com>
2022-07-27 17:58:11 +0300

    init commit

---
+++ src/main/java/com/anniken/tests_for_web_applications/TestsForWebApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.anniken.tests_for_web_applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsForWebApplicationsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestsForWebApplicationsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/anniken/tests_for_web_applications/TestsForWebApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anniken.tests_for_web_applications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsForWebApplicationsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
